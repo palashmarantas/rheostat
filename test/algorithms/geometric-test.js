1f1cf9f5348dd79c3a680d2a8056830ab8ea65af
palashmarantas <91466368+palashmarantas@users.noreply.github.com>
2022-04-11 23:07:16 +0530

    Update geometric-test.js

--- test/algorithms/geometric-test.js
+++ test/algorithms/geometric-test.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import casual from 'casual';
 import geometric from '../../src/algorithms/geometric';
-/* sdf dfds bb*/
+/* sdf dfds bbf*/
 describe('geometric algorithm', () => {
  
   it('should have inverse functions for getPosition and getValue', () => {
