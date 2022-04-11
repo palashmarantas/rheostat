d77aa5d7def9466a45703d9480e42f6e22a45a66
palashmarantas <91466368+palashmarantas@users.noreply.github.com>
2022-04-11 23:17:30 +0530

    Update geometric-test.js

--- test/algorithms/geometric-test.js
+++ test/algorithms/geometric-test.js
@@ -51,8 +51,7 @@ describe('geometric algorithm', () => {
 
   it('should should detect multiple of 2', () => {
     const number = Math.floor(Math.random() * 3);
-    const result = number % 2;
-    assert.notEqual(result, 1);
+    assert.notEqual(number, 1);
   });
 
   it('should should detect multiple of 3', () => {
